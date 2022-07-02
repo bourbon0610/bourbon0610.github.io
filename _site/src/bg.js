f0950af362c21bb9909fb9343fbeb14f84127725
Se-eun <bourbon0610@gmail.com>
2022-07-02 21:03:31 +0900

    추가

--- _site/src/bg.js
+++
@@ -1,6 +0,0 @@
-const bgimage = document.createElement("img");
-
-bgimage.src = "https://source.unsplash.com/category/background"
-bgimage.classList.add("img");
-
-document.body.appendChild(bgimage);
